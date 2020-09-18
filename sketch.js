61906ee2a3a72c8b8fbd67b879ac592b5eeae212
DesaiSwara <68277364+DesaiSwara@users.noreply.github.com>
2020-09-18 15:55:31 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,53 @@
-var dog, happyDog,database,foodS,foodStock;
+var dog,dogImg, happyDog,happyDogImg,database,foodS=0,foodStock;
 
 function preload()
 {
-  dog=loadImage("images/dogImg.png")
-  happyDog=loadImage("images/dogImg1.png");
+  dogImg=loadImage("./images/dogImg.png")
+  happyDogImg=loadImage("./images/dogImg1.png");
 }
 
 function setup() {
-	createCanvas(500, 500);
+  createCanvas(750, 650);
+  database = firebase.database();
+  console.log(database);
   
   dog=createSprite(200,200,50,50);
-  dog.addImage(happyDog);
+  dog.addImage(dogImg);
   dog.scale=0.5;
-}
 
-
-function draw() {  
-
-  drawSprites();
   
 
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
 }
 
 
+function draw() { 
+  background(46, 139, 87); 
 
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDogImg);
+  }
+  drawSprites();
+
+  textSize(12);
+  fill("white");
+  text("Food remaining :" +foodS,350,500)
+}
+
+function readStock(data) {
+   foodS=data.val();
+}
+
+function writeStock(x) {
+
+  if (x<=0) {
+    x=0
+  }else{
+    x=x-1
+  }
+   database.ref('/').update({
+     Food:x
+   })
+}
