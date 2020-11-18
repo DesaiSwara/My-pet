365deffa37b60e7c4078332e3ddfdc785b261cea
DesaiSwara <68277364+DesaiSwara@users.noreply.github.com>
2020-11-18 15:51:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var dog,dogImg, happyDog,happyDogImg,database,foodS=0,foodStock;
-var feedPet,addFood,fedTime,lastFed,foodObj,milk,hour,addFoodS;
+var feedPet,addFood,fedTime,lastFed,milk,addFoodS;
+var foodObj;
 
 function preload()
 {
@@ -8,10 +9,23 @@ function preload()
 }
 
 function setup() {
-  createCanvas(750, 650);
-  foodObj=new Food (50,50,200,200); 
   database = firebase.database();
-  console.log(database);
+  createCanvas(800, 700);
+  foodObj=new Food (50,50,200,200); 
+
+  var title=createElement('h2');
+        title.html("Virtual Pet");
+        title.position(500,0);
+        var input=createInput("Name");
+        var button=createButton("Play");
+      
+        input.position(500,50);
+        button.position(500,100);
+        button.mousePressed(function(){
+            input.position(500,440)
+;            button.hide();
+            
+        }) 
   
   dog=createSprite(200,200,50,50);
   dog.addImage(dogImg);
@@ -32,44 +46,37 @@ function setup() {
 
 function draw() { 
   background(46, 139, 87); 
+  foodObj.display();
+  
 
   fedTime=database.ref('fedTime');
   fedTime.on("value",function(data){
     lastFed=data.val();
   })
 
-  drawSprites();
-
   fill(255,255,254);
   textSize(15);
-  if (lastFed>=12) {
-    text("Last Feed:"+ lastFed%12 + "PM",350,30)
-  }else if(lastFed===0) {
-    text("Last Feed: 12 AM",350,30);
-  }else{
-    text("Last Feed:"+ lastFed+"AM",350,30)
-  }
-
+  if(lastFed>=12){
+    text("Last Feed : "+ lastFed%12 + " PM", 350,30);
+   }else if(lastFed==0){
+     text("Last Feed : 12 AM",350,30);
+   }else{
+     text("Last Feed : "+ lastFed + " AM", 350,30);
+   }
+ 
   fill("white");
   textSize(12);
-  text("Food remaining :" +foodS,550,30)
+  text("Food remaining :" +foodS,550,30);
+
+  drawSprites();
 }
 
 function readStock(data) {
    foodS=data.val();
+   foodObj.updateFoodStock(foodS)
 }
 
-function writeStock(x) {
 
-  if (x<=0) {
-    x=0
-  }else{
-    x=x-1
-  }
-   database.ref('/').update({
-     Food:x
-   })
-}
 
 function addFoodS(){
   foodS++;
@@ -78,12 +85,15 @@ function addFoodS(){
   })
 }
 
-function feedDog () {
-  dog.addImage(happyDog);
+function feedDog(){
+  dog.addImage(happyDogImg);
 
   foodObj.updateFoodStock(foodObj.getFoodStock()-1);
   database.ref('/').update({
     Food:foodObj.getFoodStock(),
-    fedTime:hour()
+    FeedTime:hour()
   })
 }
+
+  
+  
